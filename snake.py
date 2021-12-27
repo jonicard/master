3bf5cd488707c08a13c7e9df1590f77be05b5af8
Jonny <jonicard@live.com>
2021-12-27 05:05:01 -0500

    snake

--- snake.py
+++ snake.py
@@ -1,23 +1,40 @@
 #snake.py
 
-import curses
+import pygame, sys, random
+from pygame.math import Vector2
 
+class FRUIT:
+    def __init__(self):
+        self.x=random.randint(0, cell_number -1)
+        self.y=random.randint(0, cell_number -1)
+        self.pos=Vector2(self.x,self.y)
+
+    def draw_fruit(self):
+        fruit_rect=pygame.Rect(self.pos.x*cell_size,self.pos.y*cell_size,cell_size,cell_size)
+        pygame.draw.rect(screen,(126,166,114),fruit_rect)
+
+        
+pygame.init()
+cell_size=20
+cell_number=10
+screen=pygame.display.set_mode((cell_size*cell_number,cell_size*cell_number))
+clock=pygame.time.Clock()
+test_surface= pygame.Surface((100,200))
+test_rect=test_surface.get_rect(center=(200,250))
 #setup window
-
-curses.initscr()
-win= curses.newwin(20,60,0,0)#y,x
-win.keypad(1)
-curses.noecho
-curses.curs_set(0)
-win.border(0)
-win.nodelay(1)
 
 #game logic
 score=0
-
+x_pos=200
+fruit=FRUIT()
 while True:
-    event=win.getch()
-    # ...
-
-curses.endwin()
-print('Final score: ', score)
+    for event in pygame.event.get():
+         if event.type==pygame.QUIT:
+             pygame.quit()
+             sys.exit()
+    
+    screen.fill((175,215,70))
+    fruit.draw_fruit()
+    pygame.display.update()
+    clock.tick(60)
+    
